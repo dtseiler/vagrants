bb6cc2aeae0628cb062796ad98519302eb51f297
Don Seiler <don@seiler.us>
2020-04-22 16:35:17 -0500

    Obsolete file, see Vagrantfile

--- upgrade_testing/9.6/template_change.sql
+++
@@ -1,4 +0,0 @@
-update pg_database set datistemplate = FALSE where datname = 'template1';
-drop database template1;
-update pg_database set datistemplate = TRUE where datname = 'template1';
-update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'test';
