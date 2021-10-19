c8a36b540bc43ac0e1bb40f35b9a09e9857952f3
Don Seiler <don@seiler.us>
2021-10-19 12:09:27 -0500

    Updates for logical replication

---
+++ logical_replica/gen_data.sql
@@ -0,0 +1,21 @@
+CREATE TABLE orders(
+  order_id      INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email         VARCHAR(40) NOT NULL,
+  created_at    TIMESTAMP
+);
+
+INSERT INTO orders(email, created_at)
+SELECT
+  'order_' || seq || '@' || (
+    CASE (RANDOM() * 2)::INT
+      WHEN 0 THEN 'gmail'
+      WHEN 1 THEN 'hotmail'
+      WHEN 2 THEN 'yahoo'
+    END
+  ) || '.com' AS email,
+  timestamp '2016-01-10 20:00:00' +
+       random() * (now() - timestamp '2016-01-10 20:00:00')
+FROM GENERATE_SERIES(1, 200000) seq;
+
+CREATE INDEX orders_email_idx ON orders (email);
+CREATE INDEX orders_created_at_idx ON orders (created_at);
