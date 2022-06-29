c1a7bef90f56442369949b46a3f5c97b20c531b1
Don Seiler <don@seiler.us>
2022-06-29 17:48:20 -0500

    Updates and indent fixes

--- pg14_upgrade_test/gen_data.sql
+++ pg14_upgrade_test/gen_data.sql
@@ -1,8 +1,13 @@
 CREATE TABLE orders(
-  order_id      INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  order_id      INT GENERATED ALWAYS AS IDENTITY,
   email         VARCHAR(40) NOT NULL,
   created_at    TIMESTAMP NOT NULL DEFAULT now()
-);
+) PARTITION BY RANGE (created_at);
+
+CREATE INDEX orders_email_idx ON orders (email);
+CREATE INDEX orders_created_at_idx ON orders (created_at);
+
+SELECT partman.create_parent('public.orders', 'created_at', 'native', 'monthly', p_start_partition := '2016-01-01');
 
 INSERT INTO orders(email, created_at)
 SELECT
@@ -17,5 +22,3 @@ SELECT
        random() * (now() - timestamp '2016-01-10 20:00:00')
 FROM GENERATE_SERIES(1, 1000000) seq;
 
-CREATE INDEX orders_email_idx ON orders (email);
-CREATE INDEX orders_created_at_idx ON orders (created_at);
