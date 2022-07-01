6e914e256a546474365db8608d8be73e551729ed
Don Seiler <don@seiler.us>
2022-07-01 11:50:02 -0500

    Update samples to highligh sorting issues

--- pg12-pglogical/gen_data.sql
+++ pg12-pglogical/gen_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE orders(
+CREATE TABLE orders (
   order_id      INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email         VARCHAR(40) NOT NULL,
   created_at    TIMESTAMP NOT NULL DEFAULT now()
@@ -7,10 +7,11 @@ CREATE TABLE orders(
 INSERT INTO orders(email, created_at)
 SELECT
   'order_' || seq || '@' || (
-    CASE (RANDOM() * 2)::INT
+    CASE (RANDOM() * 3)::INT
       WHEN 0 THEN 'gmail'
       WHEN 1 THEN 'hotmail'
       WHEN 2 THEN 'yahoo'
+      WHEN 3 THEN 'protonmail'
     END
   ) || '.com' AS email,
   timestamp '2016-01-10 20:00:00' +
@@ -19,3 +20,12 @@ FROM GENERATE_SERIES(1, 1000000) seq;
 
 CREATE INDEX orders_email_idx ON orders (email);
 CREATE INDEX orders_created_at_idx ON orders (created_at);
+
+-- Sort Test Data
+CREATE TABLE test_data AS SELECT x FROM (values ('a'), ('$a'), ('a$'), ('b'), ('$b'), ('b$'), ('A'), ('B')) AS l(x);
+ALTER TABLE test_data ADD PRIMARY KEY (x);
+--CREATE INDEX test_data_idx ON test_data(x);
+
+CREATE TABLE test_data2 AS SELECT x FROM (values ('"0102"'), ('0102')) AS x(x);
+ALTER TABLE test_data2 ADD PRIMARY KEY (x);
+--CREATE INDEX test_data2_idx ON test_data2(x);
